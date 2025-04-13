6fd6ecc3fda0e53cd22153f17f082780d82de18f
Alexmares <alexmaresleon@gmail.com>
2025-04-13 13:16:53 -0600

    Realaciones entre Categorias y Productos

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Product } from "../../products/entities/product.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Category {
@@ -8,5 +9,9 @@ export class Category {
 
     @Column({type: 'varchar', length: 80})
     name: string
+
+    // Una categoria va a tener multiles productos
+    @OneToMany(() => Product, (Product) => Product.category, {cascade: true})
+    products: Product[]
 
 }
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,25 @@
-export class Product {}
+import { Category } from "../../categories/entities/category.entity";
+import { Column, Entity, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({type: 'varchar', length: 80})
+    name: string
+
+    @Column({type: 'varchar', length: 200, nullable:true, default:'default.svg'})
+    image: string
+
+    @Column({type: 'decimal'})
+    price: number
+
+    @Column({type: 'int'})
+    inventory: number
+
+    @ManyToOne(()=> Category)
+    category: Category
+
+}
