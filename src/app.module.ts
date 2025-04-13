2184c90ea9fb5dc61c66ea983446400b345a8365
Alexmares <alexmaresleon@gmail.com>
2025-04-13 12:44:24 -0600

    Agregamos el CRUD de Productos

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
@@ -13,7 +14,7 @@ import { typeOrmConfig } from './config/typeorm.config';
     useFactory: typeOrmConfig,
     inject: [ConfigService]
   }), 
-  CategoriesModule],  
+  CategoriesModule, ProductsModule],  
   controllers: [AppController],
   providers: [AppService],
   
