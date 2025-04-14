5c8146b64d7aaf01de4548ebb87839c59529f4f2
Alexmares <alexmaresleon@gmail.com>
2025-04-14 13:09:22 -0600

    Se crean las entidades de Transacciones y Contenido de las trasacciones y sus relaciones OneToMany y ManyToOne

--- src/transactions/entities/transaction.entity.ts
+++ src/transactions/entities/transaction.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Product } from "../../products/entities/product.entity";
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Transaction {
@@ -12,5 +13,29 @@ export class Transaction {
     // Revisar mi version de MySQL @Column({ type: 'timestamp', default: () => "CURRENT_TIMESTAMP(6)" })
     @Column({type: 'timestamp', default:()=> "CURRENT_TIMESTAMP"})
     transactionDate: Date
+
+    @OneToMany(()=> TransactionContents, (trasaction)=> trasaction.transaction)
+    contents: TransactionContents[]
    
+}
+
+@Entity()
+export class TransactionContents {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column('int')
+    quantity: number
+
+    @Column({type: 'decimal', precision: 10, scale:2})
+    price: number
+
+    @ManyToOne(()=> Product, (product)=> product.id, {eager:true})
+    product: Product
+
+    @ManyToOne(()=> Transaction, (transaction)=> transaction.contents)
+    transaction: Transaction
+
+
+
 }
