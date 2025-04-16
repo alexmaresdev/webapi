1ce3c62a0adfc22e2c6564d6d66996c3f616d526
Alexmares <alexmaresleon@gmail.com>
2025-04-15 20:15:44 -0600

    Almacenando los productos que son parte de la venta

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -4,12 +4,14 @@ import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Transaction, TransactionContents } from './entities/transaction.entity';
 import { Repository } from 'typeorm';
+import { Product } from '../products/entities/product.entity';
 
 @Injectable()
 export class TransactionsService {
   constructor(
     @InjectRepository(Transaction) private readonly transactionRepository: Repository<Transaction>,
-    @InjectRepository(TransactionContents) private readonly transactionContentsRepository: Repository<TransactionContents>
+    @InjectRepository(TransactionContents) private readonly transactionContentsRepository: Repository<TransactionContents>,
+    @InjectRepository(Product) private readonly productRepository: Repository<Product>
 
   ){}
 
@@ -19,9 +21,27 @@ export class TransactionsService {
     await this.transactionRepository.save(transaction)
 
     for(const contents of createTransactionDto.contents){
-      await this.transactionContentsRepository.save({...contents, transaction})
-    }
 
+      // findOne en TypeORM v0.3+ ya no acepta directamente condiciones como {id: 1}, sino que requiere explícitamente la propiedad where
+      const product = await this.productRepository.findOne({ where: { id: contents.productId } })      
+
+      // Valida que el producto realmente exista antes de usarlo.
+      if (!product) {
+        // Lanza una excepción si no se encuentra.
+        throw new Error(`Producto con ID ${contents.productId} no encontrado`);
+      }
+      // Descontar del inventario la cantidad de productos vendidos
+      product.inventory -= contents.quantity
+      console.log(product)
+
+      //Luego, guarda el contenido de la transacción con confianza.
+      const transactionContent = this.transactionContentsRepository.create({
+        ...contents,
+        transaction,
+        product,
+      });
+      await this.transactionContentsRepository.save(transactionContent)
+    }
     return "Venta almacendada correctamente"
   }
 
