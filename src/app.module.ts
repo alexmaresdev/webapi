6f9e19b0c2db43bf6c38703c1ea9d4bd33f3166c
Alexmares <alexmaresleon@gmail.com>
2025-04-16 15:05:10 -0600

    Agregamos el Resource de los cupones y definimos la entidad coupon.entity.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { CategoriesModule } from './categories/categories.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { ProductsModule } from './products/products.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { CouponsModule } from './coupons/coupons.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
@@ -15,7 +16,7 @@ import { TransactionsModule } from './transactions/transactions.module';
     useFactory: typeOrmConfig,
     inject: [ConfigService]
   }), 
-  CategoriesModule, ProductsModule, TransactionsModule],  
+  CategoriesModule, ProductsModule, TransactionsModule, CouponsModule],  
   controllers: [AppController],
   providers: [AppService],
   
