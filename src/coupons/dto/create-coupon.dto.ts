e6fc4b45c612e4b9fab94e71d8130293cdd2417d
Alexmares <alexmaresleon@gmail.com>
2025-04-16 15:22:17 -0600

    Creamos el DTO para cupones y agregamos validaciones

--- src/coupons/dto/create-coupon.dto.ts
+++ src/coupons/dto/create-coupon.dto.ts
@@ -1,1 +1,17 @@
-export class CreateCouponDto {}
+import { IsDateString, IsInt, IsNotEmpty, Max, MAX, Min } from "class-validator"
+
+export class CreateCouponDto {
+   
+    @IsNotEmpty({message: 'El nombre del cupon es obligatorio'})
+    name: string
+
+    @IsNotEmpty({message: 'El descuento no puede estar vacio'})
+    @IsInt({message: 'El descuento debe estar entre 1 y 30'})
+    @Max(30,{message: 'El descuento maximo es del 30%'})
+    @Min(1, {message: 'El descuento minimo es del 1%'})
+    percentage: number
+
+    @IsNotEmpty({message: 'La fecha no puede ir vacia'})
+    @IsDateString({}, {message: 'Fecha no valida'})
+    expirationDate: Date
+}
