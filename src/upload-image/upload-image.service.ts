f93da32987130678efb27e3b113e511f0197dd2d
Alexmares <alexmaresleon@gmail.com>
2025-05-14 10:44:33 -0600

    Agregamos imagenes a los productos almacenandolas en Cloudinary

---
+++ src/upload-image/upload-image.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary, UploadApiResponse, UploadApiErrorResponse, UploadStream } from 'cloudinary';
+import { CloudinaryResponse } from './upload-image.response'
+const streamifier = require('streamifier')  
+
+@Injectable()
+export class UploadImageService {
+    uploadFile(file : Express.Multer.File): Promise<CloudinaryResponse> {
+        return new Promise<CloudinaryResponse>((resolve, reject) => {
+            const uploadStream = cloudinary.uploader.upload_stream(
+                (error, result) => {
+                    if (error) return reject(error)
+                    if (!result) {
+                        return reject(new Error('La subida a Cloudinary falló: No se obtuvo resultado y no se especificó un error.'));
+                    }
+                    resolve(result as CloudinaryResponse);
+                }
+            )
+            streamifier.createReadStream(file.buffer).pipe(uploadStream)
+        })
+    }
+}
